72f7bf05843cf2acd8284d17c7a04e876cf832fd
ImanolMalamud <imanolm93@gmail.com>
2023-05-03 17:47:02 -0300

    Customers added in back and front

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,39 +1,38 @@
-import Product from "../models/Product.js"
-import ProductStat from "../models/ProductStat.js"
-import User from "../models/User.js"
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
   try {
-    const products = await Product.find()
+    const products = await Product.find();
 
     const productsWithStats = await Promise.all(
-      products.map(async product => {
+      products.map(async (product) => {
         const stat = await ProductStat.find({
           productId: product._id,
-        })
-
-        console.log(product)
+        });
 
         // When we use Promise.all with MongoDB, we use this "product._doc" to return all the product information
         return {
           ...product._doc,
           stat,
-        }
+        };
       })
-    )
+    );
 
-    res.status(200).json(productsWithStats)
+    res.status(200).json(productsWithStats);
   } catch (error) {
-    res.satus(404).json({ message: error.messsage })
+    res.satus(404).json({ message: error.messsage });
   }
-}
+};
 
 export const getCustomers = async (req, res) => {
   try {
-    // Los clientes son usuarios con el parámetro role: "user"
-    const customers = await User.find({ role: "user" }).select("-password")
-    res.status(200).json(customers)
+    // Customers are clients with the parameter role: "user" (there are other roles such as "admin" and "superadmin")
+    // We want to avoid returning the value of the user's password
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
   } catch (error) {
-    res.satus(404).json({ message: error.messsage })
+    res.satus(404).json({ message: error.messsage });
   }
-}
+};
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,9 @@
-import express from "express"
-import { getProducts } from "../controllers/client.js"
-import { getCustomers } from "../controllers/client.js"
+import express from "express";
+import { getProducts, getCustomers } from "../controllers/client.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/products", getProducts)
-router.get("/customers", getCustomers)
+router.get("/products", getProducts);
+router.get("/customers", getCustomers);
 
-export default router
+export default router;
